e0264a7c1e02b92c2b655577803ba1b25411079a
Ogun FOAN <ogunfoan@hotmail.com>
2025-01-15 22:18:29 +0300

    updated prefabs

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,7 +4,7 @@ public class SpawnManager : MonoBehaviour
 {
     int startSpawn = 2;
     public GameObject[] spawnUnits;
-    private Vector3 spawnPoint = new Vector3( 31 ,0 , 0);
+    private Vector3 spawnPoint = new Vector3( 31 ,1 , 0);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
