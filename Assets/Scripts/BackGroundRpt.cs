9722d7ee139b41d72ac44dd385bc7e60f987db14
Ogun FOAN <ogunfoan@hotmail.com>
2025-02-15 20:59:56 +0300

    finished the unit3 and challenge3

--- Assets/Scripts/BackGroundRpt.cs
+++ Assets/Scripts/BackGroundRpt.cs
@@ -5,19 +5,19 @@ public class BackGroundRpt : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private Vector3 startPos;
     private float repeatWidth;
-    public float speed;
     void Start()
     {
-        startPos = transform.position;
-        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+        startPos = transform.position; 
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (startPos.x - repeatWidth > transform.position.x)
+        if (startPos.x - repeatWidth >= transform.position.x) 
         {
             transform.position = startPos;
+            Debug.Log(transform.position.x);
         }
     }
 }
